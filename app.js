fbebfcd7c96a2ab25aa32545f9343cd0ddfd5ada
BAIREDDY LIKITHKUMAR REDDY <80573107+likithreddy1@users.noreply.github.com>
2021-10-30 18:31:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,78 +1,82 @@
-let msgBtn = document.getElementById("msgBtn");
-let displayMsg = false;
+// setting delay time
+let toptime = document.getElementById("time1");
+let centertime = document.getElementById("time2");
+let day1 = document.getElementById("day");
+var days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+// creating function which shows the time  showtime();
+function showtime() {
+  //creating a variable which stores today date //
+  var today = new Date();
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  var day = today.getDay();
+  var date = today.getDate();
+  var year = today.getFullYear();
+  var month = today.getMonth() + 1;
+  time1.textContent = date + "/" + month + "/" + year;
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
 
-let middle = document.getElementById("middle");
-let msgContainer = document.getElementById("message-conatiner");
-
-//calculating time and displaying
-function showTime() {
-  let curDate = new Date();
-  let curDay = curDate.getDay();
-  let week = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  document.getElementById("day").innerText = week[curDay];
-
-  let hours = curDate.getHours();
-  let minutes = curDate.getMinutes();
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  let myTime = `${hours}:${minutes}`;
-  console.log(myTime);
-  document.getElementById("top").innerText = myTime;
-  document.getElementById("time").innerHTML = `${hours}<span>:</span>${minutes}`;
+  time2.textContent = hours + ":" + minutes + ":" + seconds;
+  day1.textContent = days[day];
 }
-
-setInterval(showTime, 1000);
-
-//displaying the message conatiner on click the button
+setInterval(showtime, 1000);
+//sms pogression
+let clicking = true;
 let msg = `
         <div id="msg-list">
         <p id="java">Java</p>
         <p id="html">Html</p>
-        <p id="css">Css</p>
-        </div>`;
-msgBtn.addEventListener("click", function () {
-  if (displayMsg) {
-    displayMsg = false;
-    middle.style.display = "inherit";
-    msgContainer.style.display = "none";
+        <p id="css">Css</p> </div>`;
+document.getElementById("chat").addEventListener("click", () => {
+  
+  if (clicking) {
+    document.querySelector("#msg-container").innerHTML = msg;
+    document.querySelector("#msg-container").style.display = "block";
+    document.querySelector("#middletime").style.display = "none";
+    document.querySelector("#logos").style.marginTop = "22px";
+    clicking = false;
   } else {
-    displayMsg = true;
-    middle.style.display = "none";
-    msgContainer.style.display = "inherit";
-
+    document.querySelector("#middletime").style.display = "block";
+    document.querySelector("#msg-container").style.display = "none";
+    document.querySelector("#logos").style.marginTop = "45px";
+    clicking = true;
     
-
-    msgContainer.innerHTML = msg;
-    var java = document.getElementById("java");
+  }
+  var java = document.getElementById("java");
     var html = document.getElementById("html");
     var css = document.getElementById("css");
-  }
-
-
-  //displaying the respective messages 
-  
   java.addEventListener("click", function () {
     document.getElementById("msg-list").style.display = "none";
     let javaMsg = `<p>Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.</p>`;
-    msgContainer.innerHTML = javaMsg;
+    document.querySelector("#msg-container").innerHTML = javaMsg;
   });
 
   html.addEventListener("click", function () {
     document.getElementById("msg-list").style.display = "none";
     let htmlMsg = `<p>The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.</p>`;
-    msgContainer.innerHTML = htmlMsg;
+    document.querySelector("#msg-container").innerHTML = htmlMsg;
   });
 
   css.addEventListener("click", function () {
     document.getElementById("msg-list").style.display = "none";
     let cssMsg = `<p>Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML</p>`;
-    msgContainer.innerHTML = cssMsg;
+    document.querySelector("#msg-container").innerHTML = cssMsg;
   });
 });
