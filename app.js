ab9b7319da9c3fb940914b1a46377f43f62fd1a0
HM-711 <81849211+HM-711@users.noreply.github.com>
2022-03-19 00:51:46 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-var updateTime = setInterval(showTimeDay, 1000); // update time for every 1s
+var updateTime = setInterval(showTimeDay, 1000); 
 
 
-//showing time and day
+
 function showTimeDay() {
     var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes() ; //getting time 
+    var time = today.getHours() + ":" + today.getMinutes() ; 
 
     document.getElementById('topTime').innerHTML = time
 
-    document.getElementById('time').innerHTML = time; //showing time on page
+    document.getElementById('time').innerHTML = time; 
 
-    var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];//array of days
-    document.getElementById('day').innerHTML = weekDays[today.getDay()] // showing todays day on page
-
+    var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    document.getElementById('day').innerHTML = weekDays[today.getDay()] 
 }
 
 
@@ -21,20 +20,20 @@ let innerMsg = document.getElementById('innermessage')
 let hr = document.createElement('hr')
 var msg = document.createElement('p')
 
-//showing messages window
+
 function showMsg() {
     document.getElementById('timeDisplay').style.display = 'none'
     document.getElementById('messageDisplay').style.display = 'block'
     document.getElementById('music').style.color = 'gray'
     document.getElementById('clock').style.color = 'gray'
 }
-//showing full messages after clicking on message
+
 function showJavaMsg() {
     document.getElementById('btnMessage').disabled = true
     message.style.display = 'none'
     innerMsg.style.display = 'block'
 
-    // var javamsg = document.createElement('p')
+   
     msg.innerText = "Java is a high-level programming language developed by Sun Microsystems."
     innerMsg.appendChild(msg)
     innerMsg.appendChild(hr)
@@ -46,7 +45,7 @@ function showHtmlMsg() {
     message.style.display = 'none'
     innerMsg.style.display = 'block'
     
-    // var htmlmsg = document.createElement('p')
+    
     msg.innerText = "HTML stands for Hyper Text Markup Language. HTML describes the structure of a Web page."
     innerMsg.appendChild(msg)
     innerMsg.appendChild(hr)
@@ -57,13 +56,13 @@ function showCssMsg() {
     message.style.display = 'none'
     innerMsg.style.display = 'block'
     
-    // var cssmsg = document.createElement('p')
+    
     msg.innerText = "Css is a cascading style sheet used for styling purpose."
     innerMsg.appendChild(msg)
     innerMsg.appendChild(hr)
 }
 
-//showing time when clicks on clock button
+
 function showWatch() {
     msg.innerText = ''
     document.getElementById('btnMessage').disabled = false
